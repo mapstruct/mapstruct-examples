a7472c9522d35fdd3027059a9d0a3f19e631889b
sjaakd <sjaakderksen@zonnet.nl>
2014-10-09 21:42:17 +0200

    Adding MapStruct Lombok example

---
+++ mapstruct-lombok/mapper/src/test/java/com/mycompany/mapper/SourceTargetMapperTest.java
@@ -0,0 +1,38 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.mycompany.mapper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.mycompany.dto.Source;
+import com.mycompany.entities.Target;
+
+public class SourceTargetMapperTest {
+
+    @Test
+    public void testMapping() {
+        Source s = new Source();
+        s.setTest( "5" );
+
+        Target t = SourceTargetMapper.MAPPER.toTarget( s );
+        assertEquals( 5, (long) t.getTesting() );
+    }
+}

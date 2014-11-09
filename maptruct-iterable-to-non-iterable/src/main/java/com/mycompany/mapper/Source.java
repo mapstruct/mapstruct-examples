174d8fd8ff0557ad46bad8994cf69597c9d716c2
sjaakd <sjaakderksen@zonnet.nl>
2014-11-09 18:45:40 +0100

    #iterable-non-iterable, showing how a collection can be mapped to its element

---
+++ maptruct-iterable-to-non-iterable/src/main/java/com/mycompany/mapper/Source.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.mycompany.mapper;
+
+import java.util.List;
+
+/**
+ *
+ * @author Sjaak Derksen
+ */
+public class Source {
+
+
+    private List<Integer> myIntegers;
+    private List<String> myStrings;
+
+    public List<Integer> getMyIntegers() {
+        return myIntegers;
+    }
+
+    public void setMyIntegers( List<Integer> myIntegers ) {
+        this.myIntegers = myIntegers;
+    }
+
+    public List<String> getMyStrings() {
+        return myStrings;
+    }
+
+    public void setMyStrings( List<String> myStrings ) {
+        this.myStrings = myStrings;
+    }
+
+
+}
---
+++ maptruct-iterable-to-non-iterable/src/main/java/com/mycompany/mapper/util/IterableNonInterableUtil.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.mycompany.mapper.util;
+
+import java.util.List;
+//import org.mapstruct.ap.test.selection.qualifier.annotation.TitleTranslator;
+
+/**
+ *
+ * @author Sjaak Derksen
+ */
+public class IterableNonInterableUtil {
+
+    @FirstElement
+    public <T> T first( List<T> in ) {
+        if ( in != null && !in.isEmpty() ) {
+            return in.get( 0 );
+        }
+        else {
+            return null;
+        }
+    }
+
+    @LastElement
+    public <T> T last( List<T> in ) {
+        if ( in != null && !in.isEmpty() ) {
+            return in.get( in.size() - 1 );
+        }
+        else {
+            return null;
+        }
+    }
+
+
+}

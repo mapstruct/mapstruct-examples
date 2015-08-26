bbd4621614592fe362031ccbaf1813a0fa77d2e7
Rahul Joshi <rahul.joshi@taulia.com>
2015-08-26 14:03:33 -0700

    Fixed compilation error and test error.

--- mapstruct-on-gradle/src/main/java/org/mapstruct/example/SourceTargetMapper.java
+++ mapstruct-on-gradle/src/main/java/org/mapstruct/example/SourceTargetMapper.java
@@ -18,13 +18,15 @@
  */
 package org.mapstruct.example;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mappers;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface SourceTargetMapper {
+
 
     SourceTargetMapper INSTANCE = Mappers.getMapper( SourceTargetMapper.class );
 
@@ -34,5 +36,6 @@ public interface SourceTargetMapper {
     })
     Target sourceToTarget(Source source);
 
+    @InheritInverseConfiguration
     Source targetToSource(Target target);
 }

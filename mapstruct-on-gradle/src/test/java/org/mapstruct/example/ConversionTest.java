5ba081b6fcbe5b80cdd45b84401630f72a758529
Torsten Werner <torsten.werner@westernacher.com>
2015-08-29 19:52:49 +0200

    remove unused import statements

--- mapstruct-on-gradle/src/test/java/org/mapstruct/example/ConversionTest.java
+++ mapstruct-on-gradle/src/test/java/org/mapstruct/example/ConversionTest.java
@@ -1,26 +1,23 @@
 /**
- *  Copyright 2012-2013 Gunnar Morling (http://www.gunnarmorling.de/)
- *  and/or other contributors as indicated by the @authors tag. See the
- *  copyright.txt file in the distribution for a full listing of all
- *  contributors.
+ * Copyright 2012-2013 Gunnar Morling (http://www.gunnarmorling.de/)
+ * and/or other contributors as indicated by the @authors tag. See the
+ * copyright.txt file in the distribution for a full listing of all
+ * contributors.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mapstruct.example;
 
-import org.mapstruct.example.Source;
-import org.mapstruct.example.SourceTargetMapper;
-import org.mapstruct.example.Target;
 import org.testng.annotations.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -30,67 +27,67 @@ public class ConversionTest {
     @Test
     public void shouldApplyConversions() {
         Source source = new Source();
-        source.setFoo( 42 );
-        source.setBar( 23L );
-        source.setZip( 73 );
+        source.setFoo(42);
+        source.setBar(23L);
+        source.setZip(73);
 
-        Target target = SourceTargetMapper.INSTANCE.sourceToTarget( source );
+        Target target = SourceTargetMapper.INSTANCE.sourceToTarget(source);
 
-        assertThat( target ).isNotNull();
-        assertThat( target.getFoo() ).isEqualTo(42L);
-        assertThat( target.getBar() ).isEqualTo( 23 );
-        assertThat( target.getZip() ).isEqualTo( "73" );
+        assertThat(target).isNotNull();
+        assertThat(target.getFoo()).isEqualTo(42L);
+        assertThat(target.getBar()).isEqualTo(23);
+        assertThat(target.getZip()).isEqualTo("73");
     }
 
     @Test
     public void shouldHandleNulls() {
         Source source = new Source();
-        Target target = SourceTargetMapper.INSTANCE.sourceToTarget( source );
+        Target target = SourceTargetMapper.INSTANCE.sourceToTarget(source);
 
-        assertThat( target ).isNotNull();
-        assertThat( target.getFoo() ).isEqualTo(0L);
-        assertThat( target.getBar() ).isEqualTo( 0 );
-        assertThat( target.getZip() ).isEqualTo( "0" );
+        assertThat(target).isNotNull();
+        assertThat(target.getFoo()).isEqualTo(0L);
+        assertThat(target.getBar()).isEqualTo(0);
+        assertThat(target.getZip()).isEqualTo("0");
     }
 
     @Test
     public void shouldApplyConversionsToMappedProperties() {
         Source source = new Source();
-        source.setQax( 42 );
-        source.setBaz( 23L );
+        source.setQax(42);
+        source.setBaz(23L);
 
-        Target target = SourceTargetMapper.INSTANCE.sourceToTarget( source );
+        Target target = SourceTargetMapper.INSTANCE.sourceToTarget(source);
 
-        assertThat( target ).isNotNull();
-        assertThat( target.getBaz() ).isEqualTo(42L);
-        assertThat( target.getQax() ).isEqualTo( 23 );
+        assertThat(target).isNotNull();
+        assertThat(target.getBaz()).isEqualTo(42L);
+        assertThat(target.getQax()).isEqualTo(23);
     }
 
     @Test
     public void shouldApplyConversionsForReverseMapping() {
         Target target = new Target();
-        target.setFoo( 42L );
-        target.setBar( 23 );
-        target.setZip( "73" );
+        target.setFoo(42L);
+        target.setBar(23);
+        target.setZip("73");
 
-        Source source = SourceTargetMapper.INSTANCE.targetToSource( target );
+        Source source = SourceTargetMapper.INSTANCE.targetToSource(target);
 
-        assertThat( source ).isNotNull();
-        assertThat( source.getFoo() ).isEqualTo( 42 );
-        assertThat( source.getBar() ).isEqualTo( 23 );
-        assertThat( source.getZip() ).isEqualTo( 73 );
+        assertThat(source).isNotNull();
+        assertThat(source.getFoo()).isEqualTo(42);
+        assertThat(source.getBar()).isEqualTo(23);
+        assertThat(source.getZip()).isEqualTo(73);
     }
 
     @Test
     public void shouldApplyConversionsToMappedPropertiesForReverseMapping() {
         Target target = new Target();
-        target.setQax( 42 );
-        target.setBaz( 23L );
+        target.setQax(42);
+        target.setBaz(23L);
 
-        Source source = SourceTargetMapper.INSTANCE.targetToSource( target );
+        Source source = SourceTargetMapper.INSTANCE.targetToSource(target);
 
-        assertThat( source ).isNotNull();
-        assertThat( source.getBaz() ).isEqualTo( 42 );
-        assertThat( source.getQax() ).isEqualTo( 23 );
+        assertThat(source).isNotNull();
+        assertThat(source.getBaz()).isEqualTo(42);
+        assertThat(source.getQax()).isEqualTo(23);
     }
 }

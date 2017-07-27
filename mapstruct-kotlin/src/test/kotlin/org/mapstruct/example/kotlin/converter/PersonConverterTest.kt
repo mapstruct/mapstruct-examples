03eb69055773e78f1011fac1d609e88a81ec6738
Filip Hrisafov <filip.hrisafov@gmail.com>
2017-07-27 17:56:12 +0200

    Add test for Kotlin example

---
+++ mapstruct-kotlin/src/test/kotlin/org/mapstruct/example/kotlin/converter/PersonConverterTest.kt
@@ -0,0 +1,40 @@
+package org.mapstruct.example.kotlin.converter
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mapstruct.example.kotlin.dto.PersonDto
+import org.mapstruct.example.kotlin.model.Person
+import org.mapstruct.factory.Mappers
+import java.time.LocalDate
+
+class PersonConverterTest {
+
+    @Test
+    fun testConvertToDto() {
+        val converter = Mappers.getMapper(PersonConverter::class.java)
+
+        val person = Person("Samuel", "Jackson", "0123 334466", LocalDate.of(1948, 12, 21))
+
+        val personDto = converter.convertToDto(person)
+        assertThat(personDto).isNotNull()
+        assertThat(personDto.firstName).isEqualTo("Samuel")
+        assertThat(personDto.lastName).isEqualTo("Jackson")
+        assertThat(personDto.phone).isEqualTo("0123 334466")
+        assertThat(personDto.birthdate).isEqualTo(LocalDate.of(1948, 12, 21))
+    }
+
+    @Test
+    fun testConvertToModel() {
+        val converter = Mappers.getMapper(PersonConverter::class.java)
+
+        val personDto = PersonDto("Samuel", "Jackson", "0123 334466", LocalDate.of(1948, 12, 21))
+
+        val person = converter.convertToModel(personDto)
+        assertThat(person).isNotNull()
+        assertThat(person.firstName).isEqualTo("Samuel")
+        assertThat(person.lastName).isEqualTo("Jackson")
+        assertThat(person.phoneNumber).isEqualTo("0123 334466")
+        assertThat(person.birthdate).isEqualTo(LocalDate.of(1948, 12, 21))
+    }
+
+}

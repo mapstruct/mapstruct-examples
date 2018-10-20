c7e7e1aea3b59d8b6b8eb20c320a1d6296f12d06
Johan Vergeer <johan@redgyro.com>
2018-10-20 19:37:09 +0200

    Resolve dependencies

--- mapstruct-kotlin-gradle/build.gradle.kts
+++ mapstruct-kotlin-gradle/build.gradle.kts
@@ -13,8 +13,18 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+    compile("org.mapstruct:mapstruct-jdk8:1.2.0.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.2.0.Final")
+
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testCompile("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testCompile("org.assertj:assertj-core:3.11.1")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
 }

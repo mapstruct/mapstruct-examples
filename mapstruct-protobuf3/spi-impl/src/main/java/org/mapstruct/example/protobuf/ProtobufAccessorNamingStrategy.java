8dd8efc38d12b29c1a43e840ca091597d8960b5b
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-12-01 22:33:06 +0100

    Adapt ProtobbufAccessorNamingStrategy to work properly with builders
    When the adder method is part of the protobuf builder then it's superclass it not the generated message
    Add list prefix to the element name when the enclosing element of the adder is assignable to protobuf MessageOrBuilder

--- mapstruct-protobuf3/spi-impl/src/main/java/org/mapstruct/example/protobuf/ProtobufAccessorNamingStrategy.java
+++ mapstruct-protobuf3/spi-impl/src/main/java/org/mapstruct/example/protobuf/ProtobufAccessorNamingStrategy.java
@@ -1,32 +1,41 @@
 package org.mapstruct.example.protobuf;
 
-import org.mapstruct.ap.spi.DefaultAccessorNamingStrategy;
-
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 
+import org.mapstruct.ap.spi.DefaultAccessorNamingStrategy;
+import org.mapstruct.ap.spi.MapStructProcessingEnvironment;
+
 /**
  * @author Thomas Kratz
+ * @author Filip Hrisafov
  */
 public class ProtobufAccessorNamingStrategy extends DefaultAccessorNamingStrategy {
 
-    public static final String PROTOBUF_GENERATED_MESSAGE_V3 = "com.google.protobuf.GeneratedMessageV3";
+    public static final String PROTOBUF_MESSAGE_OR_BUILDER = "com.google.protobuf.MessageOrBuilder";
     public static final String LIST_SUFFIX = "List";
+
+    protected TypeMirror protobufMesageOrBuilderType;
+
+    @Override
+    public void init(MapStructProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+
+        TypeElement typeElement = elementUtils.getTypeElement(PROTOBUF_MESSAGE_OR_BUILDER);
+        if (typeElement != null) {
+            protobufMesageOrBuilderType = typeElement.asType();
+        }
+    }
 
     @Override
     public String getElementName(ExecutableElement adderMethod) {
 
         String methodName = super.getElementName(adderMethod);
         Element receiver = adderMethod.getEnclosingElement();
-        if (receiver != null && receiver.getKind() == ElementKind.CLASS) {
-            TypeElement type = (TypeElement) receiver;
-            TypeMirror superType = type.getSuperclass();
-            if (superType != null && PROTOBUF_GENERATED_MESSAGE_V3.equals(superType.toString())) {
-                methodName += LIST_SUFFIX;
-            }
+        if (receiver != null && protobufMesageOrBuilderType != null && typeUtils.isAssignable(receiver.asType(), protobufMesageOrBuilderType)) {
+            methodName += LIST_SUFFIX;
         }
         return methodName;
     }

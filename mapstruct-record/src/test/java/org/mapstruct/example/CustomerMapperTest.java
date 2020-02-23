f69c90080cd2daa4a173afb3f1d7ecf2596ec8e9
Filip Hrisafov <filip.hrisafov@gmail.com>
2020-02-23 13:59:34 +0100

    Add example with Java 14 records

---
+++ mapstruct-record/src/test/java/org/mapstruct/example/CustomerMapperTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.example;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Filip Hrisafov
+ */
+public class CustomerMapperTest {
+
+    @Test
+    public void mapRecords() {
+        CustomerEntity customer = CustomerMapper.INSTANCE.fromRecord( new CustomerDto( "Kermit", "kermit@test.com" ) );
+
+        assertThat( customer ).isNotNull();
+        assertThat( customer.getName() ).isEqualTo( "Kermit" );
+        assertThat( customer.getMail() ).isEqualTo( "kermit@test.com" );
+    }
+}

b138453c35e24a6269c34813a1b5669b75d6d36e
Filip Hrisafov <filip.hrisafov@gmail.com>
2020-08-30 16:39:13 +0200

    Upgrade examples to 1.4.0.CR1

--- mapstruct-kotlin-gradle/build.gradle.kts
+++ mapstruct-kotlin-gradle/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.mapstruct:mapstruct:1.4.0.Beta3")
-    kapt("org.mapstruct:mapstruct-processor:1.4.0.Beta3")
+    implementation("org.mapstruct:mapstruct:1.4.0.CR1")
+    kapt("org.mapstruct:mapstruct-processor:1.4.0.CR1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.1")

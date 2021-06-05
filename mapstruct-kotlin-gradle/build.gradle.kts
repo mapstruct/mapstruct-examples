b056805b83f5e1c1bba90685708d963309c75289
Filip Hrisafov <filip.hrisafov@flowable.com>
2021-06-05 10:25:00 +0200

    Update Kotlin to latest 1.5.10 version and do not run tests on Java 16+
    Kotlin kapt currently does not work correctly with Java 16+.
    See https://youtrack.jetbrains.com/issue/KT-45545 for more info

--- mapstruct-kotlin-gradle/build.gradle.kts
+++ mapstruct-kotlin-gradle/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.61"
-    kotlin("kapt") version "1.3.61"
+    kotlin("jvm") version "1.5.10"
+    kotlin("kapt") version "1.5.10"
 }
 
 group = "org.mapstruct.examples"

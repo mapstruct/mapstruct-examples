7e73ac878907556866bdf68bb6edb171a4fddf5a
Filip Hrisafov <filip.hrisafov@gmail.com>
2022-06-18 19:28:14 +0200

    Update examples to 1.5.2.Final

--- mapstruct-kotlin-gradle/build.gradle.kts
+++ mapstruct-kotlin-gradle/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.mapstruct:mapstruct:1.5.1.Final")
-    kapt("org.mapstruct:mapstruct-processor:1.5.1.Final")
+    implementation("org.mapstruct:mapstruct:1.5.2.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.5.2.Final")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.1")

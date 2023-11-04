04fc83bfb1fa9c01cfc1938631bdd3762fcc93fa
Filip Hrisafov <filip.hrisafov@gmail.com>
2023-11-04 23:42:47 +0100

    Upgrade examples to 1.6.0.Beta1

--- mapstruct-kotlin-gradle/build.gradle.kts
+++ mapstruct-kotlin-gradle/build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.mapstruct:mapstruct:1.5.5.Final")
-    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
+    kapt("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.1")
